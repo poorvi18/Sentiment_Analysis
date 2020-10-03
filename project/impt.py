5058aaf0c8f195a81999d6d771283e12a0c743ec
poorvi18 <2019uee0126@iitjammu.ac.in>
2020-10-03 20:12:53 +0000

    base flask framework

---
+++ project/impt.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return "<h1>Home</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h1>About</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
